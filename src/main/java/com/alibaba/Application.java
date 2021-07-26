aa07cf1d3f632b9442e3f5738839de259349fe72
lishihuai <348589428@qq.com>
2021-07-26 14:34:01 +0800

    第一次提交 github

---
+++ src/main/java/com/alibaba/Application.java
@@ -0,0 +1,15 @@
+package com.alibaba;
+
+/**
+ * @package Application
+ * @Description
+ * @time 2021/7/26
+ * @auth lishihuai
+ */
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+
+}
